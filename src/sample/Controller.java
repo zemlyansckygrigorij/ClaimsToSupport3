91433dc557d2ac597e6f584be5852c954537e182
Землянский Григорий <zemlyanscky.grigorij@yandex.ru>
2019-07-12 13:46:42 +0400

    commit6

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,15 +6,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Controller {
@@ -30,10 +33,9 @@ public class Controller {
     // инициализация приложения
     @FXML
     public void initialize() {
-
-        System.out.println(" initialize");
         getListErrorFromFile();
         listError.getItems().addAll(errors );
+
         // Only allowed to select single row in the ListView.
         listError.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         textFieldUser.setText(getCurrentUser());
@@ -64,34 +66,36 @@ public class Controller {
     private String getCurrentUser() {
         String username = System.getProperty("user.name");
         return username;
-
     }
 
 
 
 
     public void SendClaim() {
-        System.out.println(" SendClaim() ");
         String error = listError.getSelectionModel().getSelectedItem();
-        System.out.println( "1");
         if(error==null){
             System.out.println( "выберите ошибку из списка");
             return;
         }
-        System.out.println( "2");
-        if(UserRepository.checkUser(textFieldUser.getText())){
+        if(!UserRepository.checkUser(textFieldUser.getText().trim())){
             System.out.println( "такого пользователя нет");
             return;
         }
-
-        System.out.println( listError.getSelectionModel().getSelectedItem());
         writeToFile();
-
-        System.out.println( getCurrentDateTime());
 
     }
     public void GetAccess() {
-        System.out.println(" GetAccess ");
+
+        runTeamViewer();
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+        createScreenShot();
     }
 
     private String getCurrentDateTime(){
@@ -106,7 +110,7 @@ public class Controller {
         FileWriter fw = null; //the true will append the new data
         try {
             fw = new FileWriter(filename,true);
-            fw.write(getCurrentDateTime()+" "+getCurrentUser() +" "+ listError.getSelectionModel().getSelectedItem()+" "+additionalDescription.getText()+ " \n");//appends the string to the file
+            fw.write(getCurrentDateTime()+" "+textFieldUser.getText() +" "+ listError.getSelectionModel().getSelectedItem()+" "+additionalDescription.getText()+ " \n");//appends the string to the file
             fw.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -114,8 +118,33 @@ public class Controller {
 
 
     }
+    private void runTeamViewer(){
+        ProcessBuilder p = new ProcessBuilder();
+        p.command("C:\\Program Files\\TeamViewerQS.exe");
 
+        try {
+            p.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
+    private void createScreenShot(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd hh mm ss a");
+        Calendar now = Calendar.getInstance();
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+        BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        try {
+            ImageIO.write(screenShot, "JPG", new File("d:\\"+formatter.format(now.getTime())+".jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }

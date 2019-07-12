94db57299c8b07b7bd982361fbb1989686d12e68
Землянский Григорий <zemlyanscky.grigorij@yandex.ru>
2019-07-12 14:19:18 +0400

    commit7

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -19,6 +19,16 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.*;
+import java.io.*;
+
+
+import java.util.Properties;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
 
 public class Controller {
     public Button buttonGetAccess;
@@ -143,6 +153,86 @@ public class Controller {
             ImageIO.write(screenShot, "JPG", new File("d:\\"+formatter.format(now.getTime())+".jpg"));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private void sendMail(){
+        // Сюда необходимо подставить адрес получателя сообщения
+        String to = "sendToMailAddress";
+        String from = "sendFromMailAddress";
+        // Сюда необходимо подставить SMTP сервер, используемый для отправки
+        String host = "smtpserver.yourisp.net";
+
+        // Создание свойств, получение сессии
+        Properties props = new Properties();
+
+        // При использовании статического метода Transport.send()
+        // необходимо указать через какой хост будет передано сообщение
+        props.put("mail.smtp.host", host);
+        // Включение debug-режима
+        props.put("mail.debug", "true");
+        //Включение авторизации
+        props.put("mail.smtp.auth", "true");
+        // Получение сессии
+        Session session = Session.getInstance(props);
+
+        try {
+            // Получение объекта транспорта для передачи электронного сообщения
+            Transport bus = session.getTransport("smtp");
+
+            // Устанавливаем соединение один раз
+            // Метод Transport.send() отсоединяется после каждой отправки
+            //bus.connect();
+            // Обычно для SMTP сервера необходимо указать логин и пароль
+            bus.connect("smtpserver.yourisp.net", "username", "password");
+
+            // Создание объекта сообщения
+            Message msg = new MimeMessage(session);
+
+            // Установка атрибутов сообщения
+            msg.setFrom(new InternetAddress(from));
+            InternetAddress[] address = {new InternetAddress(to)};
+            msg.setRecipients(Message.RecipientType.TO, address);
+            // Парсинг списка адресов разделённых пробелами. Строгий синтаксис
+            msg.setRecipients(Message.RecipientType.CC,
+                    InternetAddress.parse(to, true));
+            // Парсинг списка адресов разделённых пробелами. Более мягкий синтаксис.
+            msg.setRecipients(Message.RecipientType.BCC,
+                    InternetAddress.parse(to, false));
+
+            msg.setSubject("Тест отправки E-Mail с помощью Java");
+            msg.setSentDate(new Date());
+
+            // Установка контента сообщения и отправка
+            setTextContent(msg);
+            msg.saveChanges();
+            bus.sendMessage(msg, address);
+
+            setMultipartContent(msg);
+            msg.saveChanges();
+            bus.sendMessage(msg, address);
+
+            setFileAsAttachment(msg, "C:/WINDOWS/CLOUD.GIF");
+            msg.saveChanges();
+            bus.sendMessage(msg, address);
+
+            setHTMLContent(msg);
+            msg.saveChanges();
+            bus.sendMessage(msg, address);
+
+            bus.close();
+
+        }
+        catch (MessagingException mex) {
+            // Печать информации обо всех возможных возникших исключениях
+            mex.printStackTrace();
+            // Получение вложенного исключения
+            while (mex.getNextException() != null) {
+                // Получение следующего исключения в цепочке
+                Exception ex = mex.getNextException();
+                ex.printStackTrace();
+                if (!(ex instanceof MessagingException)) break;
+                else mex = (MessagingException)ex;
+            }
         }
     }
 

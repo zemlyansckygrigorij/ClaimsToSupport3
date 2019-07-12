735849701c37e5f449dc17920572fa122f59f50d
Землянский Григорий <zemlyanscky.grigorij@yandex.ru>
2019-07-12 09:21:19 +0400

    commit2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public Button buttonGetAccess;
+    public Button buttonSendClaim;
+    public TextField textFieldUser;
+    public ListView listUsers;
+    public TextArea additionalDescription;
+
+    
+    public void SendClaim() {
+        System.out.println(" SendClaim() ");
+    }
+    public void GetAccess() {
+        System.out.println(" GetAccess ");
+    }
 }
